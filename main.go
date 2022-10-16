b604a9e961452cf7cf1e547d11c166d6de9a2d1c
Yaushiro Suzuki <yasszu@yahoo.com>
2022-10-16 21:42:31 +0900

    add NewRouter()

--- main.go
+++ main.go
@@ -4,27 +4,12 @@ import (
 	"log"
 	"net/http"
 	"time"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 	conf := NewConf()
 	h := NewHandler()
-
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Recoverer)
-	r.Use(middleware.RequestID)
-	r.Get("/health", h.Health)
-	r.Route("/api", func(r chi.Router) {
-		r.Route("/v1", func(r chi.Router) {
-			r.Get("/hello", h.Hello)
-		})
-	})
-	r.NotFound(h.ServeFile)
+	r := NewRouter(h)
 
 	srv := &http.Server{
 		Handler:      r,
---
+++ router.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func NewRouter(h *Handler) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
+
+	r.Get("/health", h.Health)
+	r.Route("/api", func(r chi.Router) {
+		r.Route("/v1", func(r chi.Router) {
+			r.Get("/hello", h.Hello)
+		})
+	})
+
+	r.NotFound(h.ServeFile)
+	return r
+}
